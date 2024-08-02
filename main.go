de2a1a74cb1e94b2614c8c851b137f3e5494ccba
mohammad jamshidian <jamshidian9898@gmail.com>
2024-08-02 15:46:22 +0330

    add simple http server and return static response

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:8880")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	incomingMessage := make([]byte, 1024)
+	_, err = conn.Read(incomingMessage)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(incomingMessage))
+
+	httpResponse := `HTTP/1.1 200 OK
+Date: Mon, 27 Jul 2009 12:28:53 GMT
+Server: Apache/2.2.14 (Win32)
+Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT
+Content-Type: text/html
+Connection: Closed
+
+<html>
+<body>
+<h1>Hello, World!</h1>
+</body>
+</html>`
+
+	_, err = conn.Write([]byte(httpResponse))
+	if err != nil {
+		panic(err)
+	}
+
+}
